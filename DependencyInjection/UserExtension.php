f2f590276d98c9e32f0df47fb9dc251909b8472a
fernando <fer@madforwebs.com>
2018-05-27 18:11:54 +0200

    initial commit

--- DependencyInjection/UserExtension.php
+++ DependencyInjection/UserExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace MFW\UserBundle\DependencyInjection;
+namespace MadForWebs\UserBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
